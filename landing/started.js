54181fd3f0eef9aed339e1ea0bca9d449442d555
Nirmitjatana <nirmitjatana38@gmail.com>
2021-05-01 21:33:23 +0530

    room name in local storage

--- landing/started.js
+++ landing/started.js
@@ -23,7 +23,7 @@ $('.joinmeeting').click(function (e) {
         postForm('https://alt.mayankkumar.tech/rooms/create')
         .then(data => { 
             
-            // console.log(data.body)
+            console.log(data.body)
         localStorage.setItem("roomID",data.body.room_id);
         // localStorage.setItem("myID",data.body.participant.participant_id);
 
@@ -59,14 +59,15 @@ $('.joinmeeting').click(function (e) {
         postForm('https://alt.mayankkumar.tech/rooms/join')
         .then(data => { 
             
-            // console.log(data.body)
+        // console.log(data.body)
         localStorage.setItem("roomID",data.body.room.room_id);
+        localStorage.setItem("roomname",data.body.room.name);
         localStorage.setItem("myID",data.body.participant.participant_id);
 
         location.href = '../app.html';
         })
         .catch(error => console.error(error))
-        console.log(e);
+        // console.log(e);
         function postForm(url) {
             // let formData = new FormData(form);
             var object = {};
